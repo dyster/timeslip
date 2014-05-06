3f884a982fb951a404fccc2585059f6da39e2b43
Johan Sonesson <johan@traintech.eu>
2014-05-06 11:31:49 +0200

    login redirect

--- app/controllers/SessionController.php
+++ app/controllers/SessionController.php
@@ -29,7 +29,7 @@ class SessionController extends ControllerBase
                         'remember' => $this->request->getPost('remember')
                     ));
 
-                    return $this->response->redirect('users');
+                    return $this->response->redirect('times');
                 }
             }
         } catch (AuthException $e) {
